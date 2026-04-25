8a2f2d25c9488382add36bc255152c3241b92958
souvik666 <peacedynasty0@gmail.com>
2026-04-25 15:13:19 +0530

    chore: bump version to 0.1.1 and remove storage exports from package init

--- src/protokol/__init__.py
+++ src/protokol/__init__.py
@@ -1,10 +1,9 @@
 from .core.step import AbstractStep
 from .core.flow import AbstractFlow
 from .core.types import RunPlan, StepContext
-from .core.storage import AbstractStorage, FileStorage
 from .engine.runner import Engine
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 __all__ = [
     "AbstractStep",
